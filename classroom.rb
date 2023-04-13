30735b14624eb1a48d52f2cef66677f042955277
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-13 16:11:07 -0400

    Sets the classroom for the student when adding a student to a classroom

--- classroom.rb
+++ classroom.rb
@@ -1,8 +1,13 @@
 class Classroom
-  attr_accessor :label, :student
+  attr_accessor :label, :students
 
   def initialize(label)
     @label = label
     @student = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
   end
 end
