ba061fd309b9379166776ddc394e7d3204d9123d
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 15:55:10 -0400

    Add list all rentals method

--- app.rb
+++ app.rb
@@ -33,4 +33,19 @@ class App
     end
     puts "\n"
   end
+
+  def list_all_rental
+    puts "\n"
+    if @rentals_list.empty?
+      puts 'The rental list is empty.'
+    else
+      print 'ID of the person : '
+      person_id = gets.chomp.to_i
+      puts "Rentals:\n"
+      @rentals_list.each_with_index do |value, _index|
+        puts "Date: #{value.date}, Book \"#{value.book.title}\" by #{value.book.author}" if value.person.id == person_id
+      end
+    end
+    puts "\n"
+  end
 end
