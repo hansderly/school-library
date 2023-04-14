33fc22b48edddda71b04d554dc9764f8cd3c959c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 15:57:20 -0400

    Implement add a person method

--- app.rb
+++ app.rb
@@ -83,4 +83,15 @@ class App
     puts 'Student created successfully'
     puts "\n"
   end
+
+  def create_person
+    puts "\n"
+    print 'Do you want to create a Student (1) or a Teacher (2) [Input the number]: '
+    choice = gets.chomp
+    if choice == '1'
+      create_student
+    else
+      create_teacher
+    end
+  end
 end
