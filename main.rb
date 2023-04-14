f6a50754184a59c5848dc375b16d962416ebd9a4
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 16:00:52 -0400

    Add list of action a user can take

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
 require_relative './app'
 
+ACTIONS = {
+  1 => :list_all_books,
+  2 => :list_all_people,
+  3 => :create_person,
+  4 => :create_book,
+  5 => :create_rental,
+  6 => :list_all_rental
+}.freeze
+
 def display_menu
   puts 'Please choose an option by entering a number:'
   puts '1 - List all book'
