ab9a3ce87e2bdac118f2ff04417e31f95322ce8b
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 16:02:47 -0400

    Add main medthod

--- main.rb
+++ main.rb
@@ -19,3 +19,24 @@ def display_menu
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
 end
+
+def main
+  puts "Welcome to School Library App!\n\n"
+  app = App.new
+  choice = 0
+  while choice != 7
+    display_menu
+    choice = gets.chomp.to_i
+    if choice == 7
+      puts 'Thank you for using this app!'
+      exit
+    end
+    method_name = ACTIONS[choice]
+    if method_name.nil?
+      puts "\nYour choice is incorrect. Must between 1-7. Please try again...\n\n"
+    else
+      method_tocall = app.method(method_name)
+      method_tocall.call
+    end
+  end
+end
