f800990a3001697eed487894d9ea6b2ee09e23b4
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 15:57:44 -0400

    Implement add a book method

--- app.rb
+++ app.rb
@@ -94,4 +94,17 @@ class App
       create_teacher
     end
   end
+
+  def create_book
+    puts "\n"
+    puts 'Please provide these information :'
+    print 'Title: '
+    title = gets.chomp
+    print 'Author : '
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    @books_list << new_book
+    puts 'Book created successfully'
+    puts "\n"
+  end
 end
