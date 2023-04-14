51fd5816bb1daca37712b8ee71e58321f0a8bb2b
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-14 15:55:51 -0400

    Implement add teacher method

--- app.rb
+++ app.rb
@@ -48,4 +48,20 @@ class App
     end
     puts "\n"
   end
+
+  def create_teacher
+    puts "\n"
+    puts 'Please provide these information :'
+    print 'Age : '
+    age = gets.chomp
+    print 'Name : '
+    name = gets.chomp
+    print 'specialization : '
+    specialization = gets.chomp
+
+    new_teacher = Teacher.new(age, specialization, name)
+    @people_list << new_teacher
+    puts 'Teacher created successfully'
+    puts "\n"
+  end
 end
