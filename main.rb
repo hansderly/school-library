46910b9adcb6b79d245786f39e2f66bf67f77c0c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-18 12:27:21 -0400

    Run selection from app.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
-require_relative './app'
-require './modules/menu'
+require_relative './selection'
 
 ACTIONS = {
   1 => :list_all_books,
@@ -10,29 +9,11 @@ ACTIONS = {
   6 => :list_all_rental
 }.freeze
 
-class Main
-  include Menu
-  def main
-    puts "Welcome to School Library App!\n\n"
-    app = App.new
-    choice = 0
-    while choice != 7
-      display_menu
-      choice = gets.chomp.to_i
-      if choice == 7
-        puts 'Thank you for using this app!'
-        exit
-      end
-      method_name = ACTIONS[choice]
-      if method_name.nil?
-        puts "\nYour choice is incorrect. Must between 1-7. Please try again...\n\n"
-      else
-        method_tocall = app.method(method_name)
-        method_tocall.call
-      end
-    end
-  end
+def main
+  puts "Welcome to School Library App!\n\n"
+
+  selection = Selection.new
+  selection.app
 end
 
-main = Main.new
 main
