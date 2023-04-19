0e4056b6f3c03e6e57331f04dce2b79db7eddc61
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-19 12:49:22 -0400

    return empty array instead books

--- data/read_data.rb
+++ data/read_data.rb
@@ -2,8 +2,7 @@ require 'json'
 
 module ReadData
   def read_books
-    books = []
-    return books unless File.exist?('./data/books.json') && !File.zero?('./data/books.json')
+    return [] unless File.exist?('./data/books.json') && !File.zero?('./data/books.json')
 
     JSON.parse(File.read('./data/books.json'))
   end
