62bd4c4510abc40a325d4f18d7cf16eda649c811
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-19 20:07:01 -0400

    List all rentals for a specific person

--- modules/list_all_rentals.rb
+++ modules/list_all_rentals.rb
@@ -8,11 +8,13 @@ module ListAllRentals
       person_id = gets.chomp.to_i
       puts "Rentals:\n"
 
-      @rentals_list.each_with_index do |value, _i|
-        p value
-        if value['person'] == person_id
-          p 'perosn'
-          p @books_list
+      @rentals_list.each do |value|
+        next unless value['person'] == person_id
+
+        @books_list.each_with_index do |book, i|
+          next unless value['book'].to_i == book['id'].to_i
+
+          puts book
         end
       end
 
