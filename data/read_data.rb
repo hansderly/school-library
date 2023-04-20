c3f9ab962d7aea68b8a67a0ba934e2d48b16e3ab
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-04-19 20:31:30 -0400

    Fix rubocop using empty? instead of zero?

--- data/read_data.rb
+++ data/read_data.rb
@@ -2,20 +2,20 @@ require 'json'
 
 module ReadData
   def read_books
-    return [] unless File.exist?('./data/books.json') && !File.zero?('./data/books.json')
+    return [] unless File.exist?('./data/books.json') && !File.empty?('./data/books.json')
 
     JSON.parse(File.read('./data/books.json'))
   end
 end
 
 def read_people
-  return [] unless File.exist?('./data/people.json') && !File.zero?('./data/people.json')
+  return [] unless File.exist?('./data/people.json') && !File.empty?('./data/people.json')
 
   JSON.parse(File.read('./data/people.json'))
 end
 
 def read_rentals
-  return [] unless File.exist?('./data/rentals.json') && !File.zero?('./data/rentals.json')
+  return [] unless File.exist?('./data/rentals.json') && !File.empty?('./data/rentals.json')
 
   JSON.parse(File.read('./data/rentals.json'))
 end
